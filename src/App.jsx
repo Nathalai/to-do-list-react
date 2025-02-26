318be7f47cf28a3e5633b38b3ef58ff45886a6be
Nathalia <nlscalza@gmail.com>
2025-02-26 16:58:03 -0300

    feat: defines basic to-do list layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Criar funcionalidade X no sistema',
+      category: 'Trabalho',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: ' Ir para a academia',
+      category: 'Pessoal',
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: 'Estudar React',
+      category: 'Estudos',
+      isCompleted: false,
+    },
+], )
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app'>
+      <h1>Lista de Tarefas</h1>
+      <div className="to-do-list">
+        {tasks.map((task) => (
+          <div className="task">
+            <div className="content">
+              <p>{ task.text }</p>
+              <p>({ task.category })</p>
+            </div>
+            <div>
+              <button>Completar</button>
+              <button>x</button>
+            </div>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
